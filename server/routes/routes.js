4cc44d2cc38d582c2bbd4d9f77e9b4db716f2aff
Emile Nsengimana <emilereas7@gmail.com>
2019-03-19 17:29:35 +0200

    [feature-164734215] allow a registered user to login

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/api/v2/auth/signup', userController.userSignup);
 router.delete('/api/v2/users/:email', userController.deleteUser);
+router.post('/api/v2/auth/login', userController.login);
 
 export default router;
